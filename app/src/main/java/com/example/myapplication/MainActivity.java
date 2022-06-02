2b346a200d8306cd34a2fc356c64c6d4873bd9e3
jin <1596604120@qq.com>
2022-06-02 11:01:42 +0800

    add some xml config file. revised MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initData();
+        initView();
+        initEvent();
+    }
+
+    private void initEvent() {
+    }
+
+    private void initView() {
+        
+    }
+
+    private void initData() {
+        
     }
 }
